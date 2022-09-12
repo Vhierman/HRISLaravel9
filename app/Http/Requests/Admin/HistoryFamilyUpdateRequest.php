8f0d8e0953be5b53388777c6357f9becd85525f0
Vhierman <achmadfirmansyah989@yahoo.co.id>
2022-09-12 09:49:17 +0700

    CRUD History Family

---
+++ app/Http/Requests/Admin/HistoryFamilyUpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HistoryFamilyUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'employees_id'                          => 'required|integer',
+            'hubungan_keluarga'                     => 'required|string|in:Istri,Suami,Anak,Ayah,Ibu',
+            'nik_history_keluarga'                  => 'required|min:16',
+            'nama_history_keluarga'                 => 'required|string',
+            'jenis_kelamin_history_keluarga'        => 'required|in:Pria,Wanita',
+            'tempat_lahir_history_keluarga'         => 'required',
+            'tanggal_lahir_history_keluarga'        => 'required|date',
+            'golongan_darah_history_keluarga'       => 'required|string|in:A,B,AB,O'
+        ];
+    }
+}
